06b4e0e2e812d1ef3e2dc4c40c894ef3cd30ed46
euniceakligo <eyakligo@st.ug.edu.gh>
2024-09-27 17:30:29 +0000

    -

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
 """
-This module contains a function to query the Reddit API
-and return the number of subscribers for a given subreddit.
+Script that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API to get the number of subscribers for a given subreddit.
-    
-    Args:
-        subreddit (str): The name of the subreddit.
-
-    Returns:
-        int: Number of subscribers if subreddit exists, 0 otherwise.
-    """
-    # Define the user agent to avoid 'Too Many Requests' errors
-    headers = {'User-Agent': 'Mozilla/5.0'}
-
-    # Send a GET request to the Reddit API to fetch subreddit information
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    response = requests.get(url, headers=headers)
-
-    # Check if the subreddit exists and if the request was successful
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data['data']['subscribers']
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
-        # If subreddit does not exist or any other error occurs
         return 0
